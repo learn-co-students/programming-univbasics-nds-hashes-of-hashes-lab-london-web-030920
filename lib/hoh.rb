a51666b94377f7abd1d84f5bed22d990dae4f90f
Julian Wakefield <julianwakefield@outlook.com>
2020-03-03 20:33:21 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,24 +13,62 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
 
 def naming_system
+
+new_hash = {
+
+label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category:{
+       label: "Class",
+       sub_category: {
+         label: "Order",
+         sub_category: {
+          # label: "Family",
+         sub_category: {
+           label: "Genus",
+           sub_category: {
+             label: "Species",
+           }
+         }
+}
+       }
+     }
+   }
+
+}
+
+
+end
+
+#label: "Kingdom", sub_category: {label: "Phylum",sub_category: {label: "Class"}}}
+# {label: "Order"}
+# {label: "Family",sub_category: {label: "Genus"}}
+# {label: "Species",sub_category: nil}}
+
+
+
+
+
+
   # Remember:
   #  Kingdom
   #  Phylum
@@ -42,4 +80,11 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
-end
+
+
+
+  #new_hash =
+  #{:label => “Kingdom”, :sub_category {:label => “Phylum”, :sub_category => {:label = "Class"}}},
+  #{:label => “Order”},
+  #{:label => “Family”, :sub_category => {:label => “Genus”}},
+  #{:label => “Species”, :sub_category => nil}
